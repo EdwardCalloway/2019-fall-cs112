aeeda2b5071469e449b8cab916db4e5931c8f094
Edward Calloway <EdwardRCalloway@gmail.com>
2019-09-15 18:02:52 -0700

    Finished project 2

--- projects/pa2/pa2/Main.cpp
+++ projects/pa2/pa2/Main.cpp
@@ -7,18 +7,41 @@ using namespace std;
 
 int main() {
 	string line;
-	ifstream myfile("tinypix.ppm");
-	myfile().open;
+	ifstream myfile ("tinypix.ppm");
+	int linechecker = 0;
 	if (myfile.is_open())
 	{
+
 		while ( getline (myfile, line) )
 		{
-			cout << line << '\n';
+			if (linechecker == 0)
+			{
+				cout << "Image Format : " << line << '\n';
+				linechecker = linechecker + 1;
+			}
+			else if (linechecker == 1)
+			{
+				cout << "Width: " << line.substr(0,1) << '\n' << "Length: " << line.substr(2,3) << '\n';
+				linechecker = linechecker + 1;
+			}
+			else if (linechecker == 2)
+			{
+				cout << "Max Pixel Value : " << line << '\n' << "Data :" << '\n';
+				linechecker = linechecker + 1;
+			}
+			else
+			{
+				cout << line << '\n';
+			}
+		
 		}
 		myfile.close();
+	
 	}
 
 	else cout << "Unable to open file";
+
+
 
 	return 0;
 }
