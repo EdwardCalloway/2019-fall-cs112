9cf059045baeedebe2c7f3974649f8467a70e5af
Edward Calloway <EdwardRCalloway@gmail.com>
2019-09-24 21:24:01 -0700

    Switched my pa 3 to match what we did in class today

--- projects/pa3/pa3/Main.cpp
+++ projects/pa3/pa3/Main.cpp
@@ -1,48 +1,15 @@
 #include <iostream>
-#include <vector>
-#include <exception>
-#include <fstream>
 #include <string>
-using namespace std;
+#include <sstream>
+#include <cmath>
+#include <exception> //exposes Exception objects
+#include <vector> //CH4 -- replaces basic C-style arrays
+#include "HelperFunctions.h"
+#include "FileHelpers.h"
+#include "PpmDocument.hpp"
 
-int main()
+int main(int argc, char* argv[])
 {
-	string filename;
-	string outputname;
-	cout << "Please Enter the name of the input file \n";
-	cin >> filename;
-	cout << "Please enter the name of the output file \n";
-	cin >> outputname;
-	string line;
-	ifstream myfile(filename);
-	fstream output(outputname);
-	int linechecker = 0;
-	if (myfile.is_open())
-	{
-		//This just puts everything into output.ppm
-		while (getline(myfile, line))
-		{
-			output << line << '\n';
-		}
-		myfile.close();
-		//Now I just have to print out the output file
-		if (output.is_open()) {
-			while (getline(output, line))
-			{	
-				cout << line << '\n';
-			}
-			output.close();
-
-		}
-
-
-
-	}
-	else {
-
-	}
+	PpmDocument doc{ "tinypix.ppm" };
 	return 0;
 }
-
-
-
